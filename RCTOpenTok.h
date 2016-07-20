3417d07be5f7c76ec441b68d09325125b215d26f
Mike Grabowski <grabbou@gmail.com>
2016-07-20 15:17:50 +0200

    Initial commit

---
+++ RCTOpenTok.h
@@ -0,0 +1,13 @@
+//
+//  RCTOpenTok.h
+//  RCTOpenTok
+//
+//  Created by Michał Grabowski on 20/07/16.
+//  Copyright © 2016 callstack. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RCTOpenTok : NSObject
+
+@end
---
+++ RCTOpenTok.m
@@ -0,0 +1,13 @@
+//
+//  RCTOpenTok.m
+//  RCTOpenTok
+//
+//  Created by Michał Grabowski on 20/07/16.
+//  Copyright © 2016 callstack. All rights reserved.
+//
+
+#import "RCTOpenTok.h"
+
+@implementation RCTOpenTok
+
+@end

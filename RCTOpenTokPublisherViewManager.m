d2cf2a8ab9a263796745785104a0ed804320958e
Mike Grabowski <grabbou@gmail.com>
2016-07-25 16:50:01 +0200

    Export props

--- RCTOpenTokPublisherViewManager.m
+++ RCTOpenTokPublisherViewManager.m
@@ -12,4 +12,12 @@ RCT_EXPORT_VIEW_PROPERTY(apiKey, NSString)
 RCT_EXPORT_VIEW_PROPERTY(sessionId, NSString)
 RCT_EXPORT_VIEW_PROPERTY(token, NSString)
 RCT_EXPORT_VIEW_PROPERTY(onSessionStartFailure, RCTDirectEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onConnected, RCTDirectEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onDisconnected, RCTDirectEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onStreamCreated, RCTDirectEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onStreamDestroyed, RCTDirectEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onConnectionCreated, RCTDirectEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onConnectionDestroyed, RCTDirectEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onUnknownError, RCTDirectEventBlock)
+
 @end

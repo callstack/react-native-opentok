60073786c1fa6a6860fc52e7b284c524c7b7c20c
Mike Grabowski <grabbou@gmail.com>
2016-07-25 16:47:48 +0200

    Add Publisher

--- RCTOpenTokPublisherView.m
+++ RCTOpenTokPublisherView.m
@@ -1,6 +1,65 @@
 @import UIKit;
 #import "RCTOpenTokPublisherView.h"
+#import "RCTEventDispatcher.h"
+@import OpenTok;
 
-@implementation RCTOpenTokenPublisherView
+@interface RCTOpenTokPublisherView () <OTSessionDelegate>
+
+@end
+
+@implementation RCTOpenTokPublisherView {
+    BOOL _isMounted;
+    OTSession *_session;
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    if (!_isMounted) {
+        [self mount];
+    }
+}
+
+- (void)mount {
+    _isMounted = YES;
+    
+    _session = [[OTSession alloc] initWithApiKey:_apiKey sessionId:_sessionId delegate:self];
+    
+    OTError *error = nil;
+    [_session connectWithToken:_token error:&error];
+    
+    if (error) {
+        _onStartFailure([NSNull null]);
+    }
+}
+
+# pragma mark - OTSession delegate callbacks
+
+- (void)sessionDidConnect:(OTSession*)session {
+    _onConnected([NSNull null]);
+}
+
+- (void)sessionDidDisconnect:(OTSession*)session {
+    _onDisconnected([NSNull null]);
+}
+
+- (void)session:(OTSession*)session streamCreated:(OTStream *)stream {
+    _onStreamCreated([NSNull null]);
+}
+
+- (void)session:(OTSession*)session streamDestroyed:(OTStream *)stream {
+    _onStreamDestroyed([NSNull null]);
+}
+
+- (void)session:(OTSession *)session connectionCreated:(OTConnection *)connection {
+    _onConnectionCreated([NSNull null]);
+}
+
+- (void)session:(OTSession *)session connectionDestroyed:(OTConnection *)connection {
+    _onConnectionDestroyed([NSNull null]);
+}
+
+- (void)session:(OTSession*)session didFailWithError:(OTError*)error {
+    _onUnknownError([NSNull null]);
+}
 
 @end
--- RCTOpenTokPublisherViewManager.m
+++ RCTOpenTokPublisherViewManager.m
@@ -1,5 +1,6 @@
 #import "RCTOpenTokPublisherViewManager.h"
 #import "RCTOpenTokPublisherView.h"
+#import "RCTComponent.h"
 
 @implementation RCTOpenTokPublisherViewManager
 
@@ -7,4 +8,8 @@
     return [RCTOpenTokPublisherView new];
 }
 
+RCT_EXPORT_VIEW_PROPERTY(apiKey, NSString)
+RCT_EXPORT_VIEW_PROPERTY(sessionId, NSString)
+RCT_EXPORT_VIEW_PROPERTY(token, NSString)
+RCT_EXPORT_VIEW_PROPERTY(onSessionStartFailure, RCTDirectEventBlock)
 @end

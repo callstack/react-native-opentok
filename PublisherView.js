e9ab17ca39db2913d036e4093514a46703bf3d23
Mike Grabowski <grabbou@gmail.com>
2016-07-27 13:34:55 +0200

    Legal stuff :fire:

--- PublisherView.js
+++ PublisherView.js
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2015-present, Callstack Sp z o.o.
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import { requireNativeComponent, View } from 'react-native';
 import React from 'react';
 
--- RCTOpenTokPublisherViewManager.m
+++ RCTOpenTokPublisherViewManager.m
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2015-present, Callstack Sp z o.o.
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 #import "RCTOpenTokPublisherViewManager.h"
 #import "RCTOpenTokPublisherView.h"
 #import "RCTComponent.h"

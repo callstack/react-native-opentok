3e7f40eaa9d13c5210772a01e5ba42a850489820
Mike Grabowski <grabbou@gmail.com>
2016-07-29 17:01:41 +0200

    Add setters

--- android/src/main/java/io/callstack/react/opentok/PublisherView.java
+++ android/src/main/java/io/callstack/react/opentok/PublisherView.java
@@ -5,7 +5,23 @@ import android.view.View;
 import com.facebook.react.uimanager.ThemedReactContext;
 
 public class PublisherView extends View {
+    private String apiKey;
+    private String sessionId;
+    private String token;
+
     public PublisherView(ThemedReactContext reactContext) {
         super(reactContext);
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
     }
 }
--- android/src/main/java/io/callstack/react/opentok/PublisherViewManager.java
+++ android/src/main/java/io/callstack/react/opentok/PublisherViewManager.java
@@ -2,6 +2,7 @@ package io.callstack.react.opentok;
 
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.annotations.ReactProp;
 
 public class PublisherViewManager extends SimpleViewManager<PublisherView> {
     @Override
@@ -12,5 +13,20 @@ public class PublisherViewManager extends SimpleViewManager<PublisherView> {
     @Override
     protected PublisherView createViewInstance(ThemedReactContext reactContext) {
         return new PublisherView(reactContext);
+    }
+
+    @ReactProp(name = "apiKey")
+    public void setApiKey(PublisherView view, String apiKey) {
+        view.setApiKey(apiKey);
+    }
+
+    @ReactProp(name = "sessionId")
+    public void setSessionId(PublisherView view, String sessionId) {
+        view.setSessionId(sessionId);
+    }
+
+    @ReactProp(name = "token")
+    public void setToken(PublisherView view, String token) {
+        view.setToken(token);
     }
 }

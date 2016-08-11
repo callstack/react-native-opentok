1d4ae4a012c993f56650eacce12dac587e43fce0
Michał Chudziak <michal.chudziak@gmail.com>
2016-08-11 15:13:52 +0200

    Session wrapper

---
+++ RCTOpenTokSession.h
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2015-present, Callstack Sp z o.o.
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+@import UIKit;
+#import "RCTEventDispatcher.h"
+#import "RCTComponent.h"
+
+@interface RCTOpenTokSession : NSObject
+
+@property (nonatomic, strong) NSString *apiKey;
+@property (nonatomic, strong) NSString *sessionId;
+@property (nonatomic, strong) NSString *token;
+
+@property (nonatomic, copy) RCTDirectEventBlock onMessageRecieved;
+
+@end
---
+++ RCTOpenTokSessionManager.m
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2015-present, Callstack Sp z o.o.
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import "RCTOpenTokSessionManager.h"
+#import "RCTOpenTokSession.h"
+
+@implementation RCTOpenTokSessionManager
+
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD();
+
+@end

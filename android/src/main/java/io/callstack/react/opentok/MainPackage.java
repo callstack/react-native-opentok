b3c98a4c889c877d6c74189620ae8ab8f0c23704
Radek Czemerys <radko93@gmail.com>
2017-08-02 23:31:01 +0200

    Fix typo in comment

--- android/src/main/java/io/callstack/react/opentok/MainPackage.java
+++ android/src/main/java/io/callstack/react/opentok/MainPackage.java
@@ -18,7 +18,7 @@ public class MainPackage implements ReactPackage {
         );
     }
 
-    // Deprecated RN 0.47
+    // Depreciated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }

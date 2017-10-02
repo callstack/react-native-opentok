3b6f2050a6f910144fded79221e27c016443cfc3
dratwas <drapich.piotr@gmail.com>
2017-10-02 16:54:51 +0200

    Add list of events to JS module

--- src/index.js
+++ src/index.js
@@ -15,6 +15,15 @@ import type {
 const listeners = {};
 
 export default {
+  events: {
+    ON_SESSION_COONECTION_CREATED: 'onSessionConnectionCreated',
+    ON_SESSION_CONNECTION_DESTROYED: 'onSessionConnectionDestroyed',
+    ON_SESSION_DID_CONNECT: 'onSessionDidConnect',
+    ON_SESSION_DID_DISCONNECT: 'onSessionDidDisconnect',
+    ON_SESSION_DID_FAIL_WITH_ERROR: 'onSessionDidFailWithError',
+    ON_SESSION_STREAM_CREATED: 'onSessionStreamCreated',
+    ON_SESSION_STREAM_DESTROYED: 'onSessionStreamDestroyed',
+  },
   connect: async (sessionId: string, token: string) => {
     await NativeModules.RNOpenTok.connect(sessionId, token);
   },

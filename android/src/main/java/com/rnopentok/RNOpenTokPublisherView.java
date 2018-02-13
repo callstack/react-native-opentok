7f0d544481bb299f7477e4d61cecf4d8428cae34
Tim Huff <timhuff@gmail.com>
2018-02-13 00:11:21 -0600

    Don't crash on disconnect

--- android/src/main/java/com/rnopentok/RNOpenTokPublisherView.java
+++ android/src/main/java/com/rnopentok/RNOpenTokPublisherView.java
@@ -72,7 +72,7 @@ public class RNOpenTokPublisherView extends RNOpenTokView implements PublisherKi
 
     private void cleanUpPublisher() {
         removeView(mPublisher.getView());
-        mPublisher.destroy();
+        // mPublisher.destroy();
         mPublisher = null;
     }
 
